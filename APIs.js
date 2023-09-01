634ce39521b3c611d218f3fb4614d1414f4b4270
Mb4y4 <d.sommy@gmail.com>
2023-09-01 21:28:17 +0300

    APIs

---
+++ APIs.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+
+const username = 'Mb4y4'
+const token = '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';
+
+
+fetch(`https://api.github.com/users/${username}/repos`, {
+    headers: {
+        authorisation: `token ${token}`,
+    },
+})
+
+.then((response) => response.json() )
+.then((repos) => {
+    repos.forEach((repo) =>{
+        console.log(`Repo: ${repo.name}`);
+        console.log(`Repo: ${repo.description}`);
+    });
+})
+
+.catch((error) => {
+    console.log('Error fetching data:', error);
+});
