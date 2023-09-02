ded8e555a634aa3b4772b2bc3f66fb3b68b3b6b7
Mb4y4 <d.sommy@gmail.com>
2023-09-02 13:31:43 +0300

    Async/Await

--- APIs.js
+++ APIs.js
@@ -1,23 +1,33 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
-const username = 'Mb4y4'
-const token = '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';
+// const username = 'Mb4y4'
+// const token = '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';
 
+// fetch(`https://api.github.com/users/${username}/repos`, {
+//     headers: {
+//         authorisation: `token ${token}`,
+//     },
+// })
 
-fetch(`https://api.github.com/users/${username}/repos`, {
-    headers: {
-        authorisation: `token ${token}`,
-    },
-})
+// .then((response) => response.json() )
+// .then((repos) => {
+//     repos.forEach((repo) =>{
+//         console.log(`Repo: ${repo.name}`);
+//         console.log(`Repo: ${repo.description}`);
+//     });
+// })
 
-.then((response) => response.json() )
-.then((repos) => {
-    repos.forEach((repo) =>{
-        console.log(`Repo: ${repo.name}`);
-        console.log(`Repo: ${repo.description}`);
-    });
-})
+// .catch((error) => {
+//     console.log('Error fetching data:', error);
+// });
 
-.catch((error) => {
-    console.log('Error fetching data:', error);
-});
+const fetchData = async ()=>{
+    try {
+        const response = await fetch(`https://api.github.com/users/Mb4y4/repos`)
+        const data = await response.json()
+        console.log(data)   
+    } catch(error) {
+        console.log(error)
+    }
+}
+fetchData()
